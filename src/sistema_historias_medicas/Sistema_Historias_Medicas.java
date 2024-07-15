e80300942bb227c1e4c1c0a4dbd759f2a9f50325
Sunlart <luisibp0410@gmail.com>
2024-07-15 01:10:40 -0400

    vergacion samuel

--- src/sistema_historias_medicas/Sistema_Historias_Medicas.java
+++ src/sistema_historias_medicas/Sistema_Historias_Medicas.java
@@ -10,8 +10,6 @@ import sistema_historias_medicas.PantallaMain;
 public class Sistema_Historias_Medicas {
     public static void main(String[] args) {
         PantallaMain pantalla = new PantallaMain();
-        System.out.println("Hola Mundo");
-        System.out.println("Si");
         System.out.println("Samuel Sam HOLA UWU");
         System.out.println("NYA");
 
