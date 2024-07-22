f1ca64a45b0ac19f2ad9e17f398f84d4d0bf5f03
Sunlart <luisibp0410@gmail.com>
2024-07-22 12:12:27 -0400

    ojala la ultima...

--- src/sistema_historias_medicas/Sistema_Historias_Medicas.java
+++ src/sistema_historias_medicas/Sistema_Historias_Medicas.java
@@ -2,6 +2,6 @@ package sistema_historias_medicas;
 
 public class Sistema_Historias_Medicas {
     public static void main(String[] args) {
-        PantallaMain pantalla = new PantallaMain();
+        PantallaMain pantalla = new PantallaMain();//Instanciando la Pantalla Principal
     }
 }
